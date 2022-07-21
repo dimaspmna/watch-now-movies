d2b9266c62f0f64656295af12da0d316ce661554
Dimaspermana293 <dimaspermana382@gmail.com>
2022-07-22 00:06:49 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let header =document.querySelector('header');
+let menu =document.querySelector('#menu-icon');
+let navbar =document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    header.classList.toggle('shadow', window.scrollY > 0);
+});
+
+// Menu navbar
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
